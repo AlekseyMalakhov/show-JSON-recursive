2fd8a42b4a3b9b4912aeabab37f0d4aafe391396
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-22 11:13:55 +0300

    it works

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,8 +1,14 @@
 import { css } from "@emotion/react";
+import { useState } from "react";
 
 const container = css({
-    display: "flex",
+    display: "block",
     marginLeft: "10px",
+    backgroundColor: "greenyellow",
+    width: "fit-content",
+    padding: "10px",
+    marginTop: "5px",
+    borderRadius: "5px",
 });
 
 type TypeItem = {
@@ -11,22 +17,37 @@ type TypeItem = {
 };
 
 export default function Item({ value, name }: TypeItem) {
+    const [showChildren, setShowChildren] = useState(true);
+
+    const toggle = () => {
+        if (showChildren) {
+            setShowChildren(false);
+        } else {
+            setShowChildren(true);
+        }
+    };
+
     if (Array.isArray(value)) {
         return (
-            <div style={{ marginLeft: "10px" }}>
-                <div>{name}</div>
-                <div style={{ marginLeft: "10px" }}>
-                    {value.map((item) => (
-                        <Item name={item.name} value={item.value} key={item.name} />
-                    ))}
+            <div style={{ marginLeft: "10px", marginTop: "10px" }}>
+                <div style={{ display: "inline-block" }}>{name}</div>
+                <div style={{ display: "inline-block", marginLeft: "10px" }}>
+                    <button onClick={toggle}>{showChildren ? "-" : "+"}</button>
                 </div>
+                {showChildren ? (
+                    <div>
+                        {value.map((item) => (
+                            <Item name={item.name} value={item.value} key={item.name} />
+                        ))}
+                    </div>
+                ) : null}
             </div>
         );
     }
     return (
         <div css={container}>
-            <div>{name}</div>
-            <div style={{ marginLeft: "10px" }}>Value: {value}</div>
+            <div style={{ display: "inline-block" }}>{name}</div>
+            <div style={{ marginLeft: "10px", display: "inline-block" }}>Value: {value}</div>
         </div>
     );
 }
